44aa44ba0ed126e09da0de7240a478bb1ba69d39
friker92 <jesusjda@gmail.com>
2015-11-23 16:39:53 +0100

    solitario paso 2

--- solitario/Assets/__Scripts/Prospector.cs
+++ solitario/Assets/__Scripts/Prospector.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 public class Prospector : MonoBehaviour {
-	static public Prospector S;
-	public Deck deck;
-	public TextAsset deckXML;
+	static public Prospector    S;
+	
+	public Deck                 deck;
+	public TextAsset            deckXML;
+
+    public Layout layout;
+    public TextAsset layoutXML;
+
 	void Awake() {
 		S = this; // Set up a Singleton for Prospector
 	}
+	
 	void Start () {
 		deck = GetComponent<Deck>(); // Get the Deck
 		deck.InitDeck(deckXML.text); // Pass DeckXML to it
+		Deck.Shuffle(ref deck.cards);    // This shuffles the deck
+
+        // The ref keyword passes a reference to deck.cards, which allows
+        // deck.cards to be modified by Deck.Shuffle()
+        layout = GetComponent<Layout>(); // Get the Layout
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
 	}
 }
+
